c09824ff8239b4f44d50c46b59fd254bef207b09
Martin Miller <miller.m.43@gmail.com>
2017-08-08 15:54:59 -0400

    location function now working

--- location.py
+++ location.py
@@ -1,10 +1,10 @@
-def coord_ip()
-				import requests 
-				import json 
-				send_url = 'http://freegeoip.net/json' 
-				r = requests.get(send_url) 
-				j = json.loads(r.text) 
-				lat = j['latitude'] 
-				lon = j['longitude']
-			 return [lat,lon]
+def coord_ip():
+	import requests 
+	import json 
+	send_url = 'http://freegeoip.net/json' 
+	r = requests.get(send_url) 
+	j = json.loads(r.text) 
+	lat = j['latitude'] 
+	lon = j['longitude']
+	return [lat,lon]
 			 
