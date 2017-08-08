40e2149f807fab88b92b795ff6d24692a77bc294
MA Miller <miller.m.43@gmail.com>
2017-08-08 15:49:53 -0400

    ip based locations

--- location.py
+++ location.py
@@ -1,7 +1,10 @@
-import requests 
-import json 
-send_url = 'http://freegeoip.net/json' 
-r = requests.get(send_url) 
-j = json.loads(r.text) 
-lat = j['latitude'] 
-lon = j['longitude']
+def coord_ip()
+				import requests 
+				import json 
+				send_url = 'http://freegeoip.net/json' 
+				r = requests.get(send_url) 
+				j = json.loads(r.text) 
+				lat = j['latitude'] 
+				lon = j['longitude']
+			 return [lat,lon]
+			 
