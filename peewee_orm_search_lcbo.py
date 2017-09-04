4a9ce1e5254372dcb2b3651569a9f78a3924dc88
MA Miller <miller.m.43@gmail.com>
2017-09-04 08:25:56 -0400

    Monday Labour Day Update

--- peewee_orm_search_lcbo.py
+++ peewee_orm_search_lcbo.py
@@ -1,52 +1,21 @@
 # coding: utf-8
 from peewee import *
 from playhouse.sqlite_ext import *
-from lcbo_db_models import *
-db = SqliteExtDatabase('lcbo_db.sqlite',threadlocals=True)
-class FTSProducts(FTSModel):
-    entry_id=IntegerField()
-    name=TextField()
-    class Meta:
-        database=db
-        
-for product in Products.select():
-    FTSProducts.create(entry_id=product.id,content=product.name)
-    
-for product in Products.select():
-    FTSProducts.create(entry_id=product.id,name=product.name)
-    
-    
-FTSProducts.create_table()
-Products.create(id=9999999,name='dummy product')
-for product in Products.select():
-    print product.id
-    print product.name
-    
-for product in Products.select():
-    print product.id
-    print product.name
-    FTSProducts.create(entry_id=product.id,name=product.name)
-    
-    
-class FTSProducts(FTSModel):
-    entry_id=IntegerField()
-    name=TextField()
-    class Meta:
-        database=db
-        
-        
-for product in Products.select():
-    print product.id
-    print product.name
-    FTSProducts.create(entry_id=product.id,name=product.name)
-    
+from lcbo_db_models import FTSProducts, Products
     
 FTSProducts.create(entry_id='1',name='dummy_name')
 search_dicts_list=[]
 for product in Products.select():
+    print('Appending product id '+str(product.id))
     search_dicts_list.append([product.id,product.name])
     
+no_of_items=len(search_dicts_list)
+print('Number of items to index: '+str(no_of_items))
+    
 search_dicts_list[0]
+j=1
 for i in search_dicts_list:
+    print('Indexing item '+str(j)+' of '+str(no_of_items))
     FTSProducts.create(entry_id=i[0],name=i[1])
+    j+=1
     
