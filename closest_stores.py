b49169796413248bb95d48cbfba63b1004b6d8de
martinm43 <miller.m.43@gmail.com>
2017-11-30 21:49:57 -0500

    valpol script and supports

--- closest_stores.py
+++ closest_stores.py
@@ -2,7 +2,7 @@
 #qpy:2
 #qpy:console
 
-def storeselect(range=5):
+def storeselect(coord='AUTO',range=5,input_lat=43.0,input_lon=-75.0):
   import os
   cwd=os.getcwd()+'/'
 
@@ -13,13 +13,16 @@ def storeselect(range=5):
 
   from location import coord_termux, coord_ip
   
-  a=coord_termux()
-  if a==None:
-	print('GPS based locations from termux unsuccessful, using IP address: ')
+  if coord=='AUTO':
+    a=coord_termux()
+    if a==None:
+  	print('GPS based locations from termux unsuccessful, using IP address: ')
 	a=coord_ip()
-  location_lat=str(a[0])
-  location_lng=str(a[1])
-
+    location_lat=str(a[0])
+    location_lng=str(a[1])
+  if coord=='MANUAL':
+    location_lat=input_lat
+    location_lng=input_lon
   str_input='SELECT name, id AS distance FROM stores ORDER BY (('+location_lat+' - latitude)*('+location_lat+' - latitude)) + (('+location_lng+' - longitude)*('+location_lng+' - longitude)) ASC LIMIT '+str(range)
   g=c.execute(str_input).fetchall()
 
